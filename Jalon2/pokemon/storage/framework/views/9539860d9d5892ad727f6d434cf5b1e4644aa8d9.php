6266e5c77b7801c254a404b9b4b1901495f14cb6
Doutreleau <56396584+Doutreleau@users.noreply.github.com>
2022-11-22 10:40:52 +0100

    Add files via upload

---
+++ Jalon2/pokemon/storage/framework/views/9539860d9d5892ad727f6d434cf5b1e4644aa8d9.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title> PokemonStop </title>
+    <link rel="stylesheet" href="css/styles.css" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.css">  
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.js"></script>
+    
+
+</head>
+<body>
+    <h1> Welcome </h1>
+    <?php echo $__env->yieldContent('content'); ?>
+    
+
+    
+</body>
+</html><?php /**PATH C:\xampp\php\www\pokemon\resources\views/template.blade.php ENDPATH**/ ?>
