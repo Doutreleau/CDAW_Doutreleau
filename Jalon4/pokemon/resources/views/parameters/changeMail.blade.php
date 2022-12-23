1d75ce9566a00f4f877f07f7bf93333376c43076
Doutreleau <luci-doutrele@sfr.fr>
2022-12-23 11:33:40 +0100

    ui for parameters

--- Jalon4/pokemon/resources/views/parameters/changeMail.blade.php
+++ Jalon4/pokemon/resources/views/parameters/changeMail.blade.php
@@ -1,33 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title> PokemonStop </title>
-    <link rel="stylesheet" href="css/styles.css" crossorigin="anonymous">
+@extends('template')
+@section('content1')
 
-</head>
-<body>
+<img class='loginPageShape'  style =" position: absolute; top:180px; " src = 'assets/img/shape3.png'></img>
 
-    <div  class='rectangleRegisterLogin'></div>
-    <h2 style =" position: absolute; top:500px; left: 32%" >Change name</h2>
-    
-    <form method="POST" action="/mailchange">
-        {{ csrf_field() }}
-        <div class="form-group">
-            <label style =" position: absolute; top:580px; left: 32%" >New mail:</label>
-            <input class="form-control" name="newMail" style =" position: absolute; top:580px; left: 42%; width: 10%">
-        </div>
+<h2 style =" position: absolute; top:300px; left: 32%" >Change mail</h2>
 
-        <div class="form-group">
-            <label for="password" style =" position: absolute; top:650px; left: 32%">Password:</label>
-            <input type="password" class="form-control" id="password" name="password" style =" position: absolute; top:650px; left: 42%; width: 10%">
-        </div>
+<form method="POST" action="/mailchange">
+    {{ csrf_field() }}
+    <div class="form-group">
+        <label style =" position: absolute; top:380px; left: 32%" >New mail:</label>
+        <input class="form-control" name="newMail" style =" position: absolute; top:380px; left: 42%; width: 10%">
+    </div>
 
-        <div class="form-group">
-            <button style="cursor:pointer; position: absolute; top:750px; left: 32%" type="submit" class="btn btn-primary">Change mail</button>
-        </div>
-    </form>
+    <div class="form-group">
+        <label for="password" style =" position: absolute; top:450px; left: 32%">Password:</label>
+        <input type="password" class="form-control" id="password" name="password" style =" position: absolute; top:450px; left: 42%; width: 10%">
+    </div>
 
-    
-</body>
-</html>
+    <div class="form-group">
+        <button style="cursor:pointer; position: absolute; top:550px; left: 32%" type="submit" class="btn btn-primary">Change mail</button>
+    </div>
+</form>
+
+
+@endsection
 
--- Jalon4/pokemon/resources/views/parameters/parametersPage.blade.php
+++ Jalon4/pokemon/resources/views/parameters/parametersPage.blade.php
@@ -1,16 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title> PokemonStop </title>
+@extends('template')
+@section('content1')
     <link rel="stylesheet" href="css/styles.css" crossorigin="anonymous">
 
-</head>
-<body>
 
-    <a href = "/namechange"> Change Name </a>
-    <a href = "/mailchange"> Change Mail </a>
-    <a href = "/passwordchange"> Change Password </a> 
+    <img class='loginPageShape'  style =" position: absolute; top:180px; " src = 'assets/img/shape3.png'></img>
+
+    <a style =" position: absolute; top:300px; left: 32%;font-family: 'Enriqueta'; font-style: normal;  font-weight: 500;  font-size: 40px; line-height: 27px;  color: #000000;" href = "/namechange"> Change Name </a>
+    <a style =" position: absolute; top:400px; left: 32%;font-family: 'Enriqueta'; font-style: normal;  font-weight: 500;  font-size: 40px; line-height: 27px;  color: #000000;" href = "/mailchange"> Change Mail </a>
+    <a style =" position: absolute; top:500px; left: 32%;font-family: 'Enriqueta'; font-style: normal;  font-weight: 500;  font-size: 40px; line-height: 27px;  color: #000000;"href = "/passwordchange"> Change Password </a> 
     
-</body>
-</html>
+    @endsection
 
